a6d710d76b1230bd7ee75c52efde9546413cf6b8
pmoukda <moukda_p@hotmail.com>
2025-10-27 22:36:53 -0400

    Add Home.vue file and changed App.vue content

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <header class="lg:px-16 px-4 bg-red-400 flex flex-wrap items-center py-4 shadow-md">
+    <div class="flex-1 flex justify-between items-center">
+      <router-link class="text-3xl font-bold text-yellow-400" to="/">SunVacay</router-link>
+    </div>
+    <label for="menu-toggle" class="pointer-cursor md:hidden block">
+      <svg class="fill-current text-white w-6 h-6"
+        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <title>menu</title>
+        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+      </svg>
+    </label>
+    <input v-model="menuOpen" id="menu-toggle" type="checkbox" class="hidden" aria-label="Menu"/>
+    <div :class="[
+          menuOpen ? 'block' : 'hidden','md:flex md:items-center md:w-auto w-full'
+          ]" id="menu">
+      <nav>
+         <ul class="md:flex items-center justify-between text-lg text-white pt-4 md:pt-0">
+          <li><router-link class="md:p-4 py-3 px-0 block hover:underline" to="/" @click="closeMenu">Accueil</router-link></li>
+          <li><router-link class="md:p-4 py-3 px-0 block hover:underline" to="/apropos" @click="closeMenu">À Propos</router-link></li>
+          <li><router-link class="md:p-4 py-3 px-0 block hover:underline" to="/forfaits" @click="closeMenu">Les Forfaits</router-link></li>
+          <div class="relative inline-block">
+            <button type="button" class="mr-2 cursor-pointer">
+              <Io5Cart class="text-2xl" />
+            </button>
+            <span class="absolute top-0 right-0 translate-x-1/2 -translate-y-1/4 bg-red-500 text-white text-xs font-bold rounded-full px-1.5 py-0.5"></span>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <Hero/>
+  <div class="font-sans min-h-screen mt-24">
+    <div class="container mx-auto p-2 max-w-screen-lg ">
+      <router-view/>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Footer/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+  import Hero from './components/Hero.vue';
+  import Footer from './components/Footer.vue';
+  import { Io5Cart  } from 'vue-icons-plus/io5';
+
+  export default {
+      components: {
+        Hero,
+        Footer,
+        Io5Cart   
+      },
+      data() {
+        return {
+        menuOpen: false
+        }
+      }, 
+      methods: {
+        toggleMenu(){
+          this.menuOpen =!this.menuOpen;
+        },
+        closeMenu() {
+          this.menuOpen = false;
+        } 
+      }
+  }
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class=" lg:px-16 px-4">
+        <div class='border border-gray-200 shadow-lg p-4 mb-10'>
+                <h2>L'art du voyage, sur mesure</h2>
+                <p class='mt-4'>Chez SunVacay, nous transformons <strong>vos rêves d’évasion en expériences inoubliables.</strong> Circuits organisés, séjours sur mesure ou escapades de dernière minute : notre équipe s’occupe de tout, pour que vous n’ayez qu’à profiter.</p>
+                <ul class='flex flex-wrap gap-6 py-8'>
+                    <li class='border border-gray-200 rounded p-2 bg-pink-100 text-pink-900'>Destinations d’exception</li>
+                    <li class='border border-gray-200 rounded p-2 bg-pink-100 text-pink-900'>Expertise sur mesure </li>
+                    <li class='border border-gray-200 rounded p-2 bg-pink-100 text-pink-900'>Accompagnement personnalisé</li>
+                </ul>
+                <div class="flex justify-end">
+                    <router-link class='btn-pink btn-small mt-5' to="/apropos">En savoir plus</router-link>
+                </div>
+            </div>
+            <div class='mt-15'>
+                <h2>Nos Destinations populaires</h2>
+                <div class="max-w-screen-lg mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6 bg-blue-100">
+                <!-- 
+                    Liste des produits
+                ))} -->
+            </div>
+            </div>
+            <div class="flex justify-end">
+                <router-link class='btn-pink btn-small mt-5' to="/forfaits">Voir plus</router-link>
+            </div>
+            <div class='mt-15 mb-6'>
+                <h2>Galerie</h2>
+                <div class="max-w-screen-lg grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                    <img class='w-full h-full aspect-4/3' src="/img/Dubai.avif" alt="dubai" />
+                    <img class='w-full h-full aspect-4/3' src="/img/egypt.webp" alt="egypt" />
+                    <img class='w-full h-full aspect-4/3' src="/img/Monaco.jpg" alt="monaco" />
+                    <img class='w-full h-full aspect-4/3' src="/img/Croisiere-Yangtse.jpg" alt="yantse" />
+                    <img class='w-full h-full aspect-4/3' src="/img/suisse.jpg" alt="suisse" />
+                    <img class='w-full h-full aspect-4/3' src="/img/tahiti-et-iles.jpg" alt="tahiti" />
+                </div>
+            </div>
+        </div>
+</template>
