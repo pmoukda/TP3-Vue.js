fbb353eafe8beff411e162bbad1338804a73159a
pmoukda <moukda_p@hotmail.com>
2025-11-03 19:32:44 -0500

    Fixing errors with git

--- src/components/Hero.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class='relative'>
-    <img class="w-full h-125 object-cover object-center mb-4" src="/img/ile-grecque.jpg" alt="Image" />
-    <div class="flex flex-col justify-center items-center absolute inset-0 bg-black/40 p-6">
-        <h1 class='text-4xl font-bold text-white'>Partez à l'aventure avec SunVacay votre agence de voyage par excellence!</h1>
-        <router-link class='btn-yellow' to="/packages">Découvrir nos forfaits</router-link>
-    </div>
-  </div>
-</template>
